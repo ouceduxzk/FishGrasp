b0947215d6e1640474b76443ab7b58ef94faec34
zaikun <zaikunxu1989@gmail.com>
2025-09-16 15:40:23 +0800

    added landmark detector

---
+++ landmarks/run_train.sh
@@ -0,0 +1,1 @@
+python3 train_landmark_model.py --mode train  --sharpness=3.0      --data_dir ./process_data        --annotations process_data/train_annotations.json        --epochs 100 --backbone=efficientnet --batch_size=128 --lr=0.001 --exp_name gaussian_$(date +%Y%m%d_%H%M%S)
